0bab4f8ab8d626ddd4c61658ee54b8284cfa45fd
mahi <mohiddinoshim@yahoo.com>
2020-05-15 15:53:38 +0600

    Set Delete book

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,6 +1,6 @@
 class BooksController < ApplicationController
 
-  before_action :set_book, except: [:index, :new, :create] 
+  before_action :set_book, except: [:index, :new, :create, :destroy] 
   before_action :authenticate_user!, except: [:show]
   before_action :is_admin, only: [:listing, :pricing, :description, :photo_upload, :amenities, :location, :update]
 
@@ -41,6 +41,7 @@ class BooksController < ApplicationController
 
   def photo_upload
     @photos = @book.photos
+
   end
 
   def amenities
@@ -58,6 +59,27 @@ class BooksController < ApplicationController
 
       redirect_back(fallback_location: request.referer)
   end
+
+  def destroy
+    @book.destroy
+
+    if @book.destroy
+        redirect_to book_path, notice: "Book Was successfully destroyed."
+      else
+        flash[:alert] = "Something Went Wrong...." 
+        render :new
+
+      end
+    
+  end
+
+  # def destroy
+  #   @test.destroy
+  #   respond_to do |format|
+  #     format.html { redirect_to tests_url, notice: 'Test was successfully destroyed.' }
+  #     format.json { head :no_content }
+  #   end
+  # end
 
   private
 
